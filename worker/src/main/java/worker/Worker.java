da6aaad01bdf9748248cb973f1cf2bf6601b3fc1
matheusmbar <code@matheusmbar.com>
2024-08-05 23:23:34 -0300

    Revert "Update Worker.java"
    This reverts commit cb8bf86bd16d39e3e162e80a5ccd026e3db2f964.

--- worker/src/main/java/worker/Worker.java
+++ worker/src/main/java/worker/Worker.java
@@ -8,14 +8,17 @@ import org.json.JSONObject;
 class Worker {
   public static void main(String[] args) {
     try {
-      Jedis redis = connectToRedis(getEnv("REDIS_HOST", "redis"), Integer.parseInt(getEnv("REDIS_PORT", "6379")));
-      Connection dbConn = connectToDB(getEnv("DB_HOST", "db"));
+      Jedis redis = connectToRedis("redis");
+      Connection dbConn = connectToDB("db");
+
       System.err.println("Watching vote queue");
+
       while (true) {
         String voteJSON = redis.blpop(0, "votes").get(1);
         JSONObject voteData = new JSONObject(voteJSON);
         String voterID = voteData.getString("voter_id");
         String vote = voteData.getString("vote");
+
         System.err.printf("Processing vote for '%s' by '%s'\n", vote, voterID);
         updateVote(dbConn, voterID, vote);
       }
@@ -25,10 +28,25 @@ class Worker {
     }
   }
 
-  // ... (other methods remain the same)
+  static void updateVote(Connection dbConn, String voterID, String vote) throws SQLException {
+    PreparedStatement insert = dbConn.prepareStatement(
+      "INSERT INTO votes (id, vote) VALUES (?, ?)");
+    insert.setString(1, voterID);
+    insert.setString(2, vote);
 
-  static Jedis connectToRedis(String host, int port) {
-    Jedis conn = new Jedis(host, port);
+    try {
+      insert.executeUpdate();
+    } catch (SQLException e) {
+      PreparedStatement update = dbConn.prepareStatement(
+        "UPDATE votes SET vote = ? WHERE id = ?");
+      update.setString(1, vote);
+      update.setString(2, voterID);
+      update.executeUpdate();
+    }
+  }
+
+  static Jedis connectToRedis(String host) {
+    Jedis conn = new Jedis(host);
 
     while (true) {
       try {
@@ -48,6 +66,7 @@ class Worker {
     Connection conn = null;
 
     try {
+
       Class.forName("org.postgresql.Driver");
       String url = "jdbc:postgresql://" + host + "/postgres";
 
@@ -72,13 +91,6 @@ class Worker {
     System.err.println("Connected to db");
     return conn;
   }
-
-  static String getEnv(String name, String defaultValue) {
-    String value = System.getenv(name);
-    return value != null ? value : defaultValue;
-  }
-
-  // ... (sleep method remains the same)
 
   static void sleep(long duration) {
     try {
