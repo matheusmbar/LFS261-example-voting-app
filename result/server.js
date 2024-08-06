d1ff17188ac38062c47a335e24f3860aadd571ae
matheusmbar <code@matheusmbar.com>
2024-08-05 23:23:35 -0300

    Revert "Update server.js attempted fix of pg.connect function not existing bug"
    This reverts commit 3ff3da5f3b6e21e600ddef130582ac0beaf93ad2.

--- result/server.js
+++ result/server.js
@@ -1,33 +1,31 @@
 var express = require('express'),
     async = require('async'),
-    { Pool } = require("pg"),
+    pg = require("pg"),
     path = require("path"),
     cookieParser = require('cookie-parser'),
     bodyParser = require('body-parser'),
     methodOverride = require('method-override'),
     app = express(),
     server = require('http').Server(app),
-    io = require('socket.io')(server, {
-      transports: ['polling']
-    });
+    io = require('socket.io')(server);
+
+io.set('transports', ['polling']);
 
 var port = process.env.PORT || 4000;
 
-io.on('connection', function (socket) {
+io.sockets.on('connection', function (socket) {
+
   socket.emit('message', { text : 'Welcome!' });
+
   socket.on('subscribe', function (data) {
     socket.join(data.channel);
   });
 });
 
-const pool = new Pool({
-  connectionString: 'postgres://postgres@db/postgres'
-});
-
 async.retry(
   {times: 1000, interval: 1000},
   function(callback) {
-    pool.connect((err, client, done) => {
+    pg.connect('postgres://postgres@db/postgres', function(err, client, done) {
       if (err) {
         console.error("Waiting for db");
       }
@@ -51,15 +49,18 @@ function getVotes(client) {
       var votes = collectVotesFromResult(result);
       io.sockets.emit("scores", JSON.stringify(votes));
     }
+
     setTimeout(function() {getVotes(client) }, 1000);
   });
 }
 
 function collectVotesFromResult(result) {
   var votes = {a: 0, b: 0};
+
   result.rows.forEach(function (row) {
     votes[row.vote] = parseInt(row.count);
   });
+
   return votes;
 }
 
